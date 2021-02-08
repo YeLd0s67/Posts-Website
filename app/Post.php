7e0b1804a1d3b2cf0d40ad9c4db76ebf7d7ab421
YeLd0s67 <eldos270616@gmail.com>
2021-02-08 16:31:31 +0600

    Pushing to repo

---
+++ app/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use App\Like;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'body',
+    ];
+
+    public function likedBy(User $user){
+        return $this->likes->contains('user_id', $user->id);
+    }
+    
+    
+    public function user()
+    {
+            return $this->belongsTo(User::class);
+    }
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    //
+}
